5373a751c4d588652440763545aa9b4d885baae9
bujingyi <jingyi.bu@outlook.com>
2018-06-11 20:37:58 +0800

    estimator model

---
+++ cnn_acceptor/estimator_model.py
@@ -0,0 +1,26 @@
+from cnn_acceptor import resnet_model
+
+
+def estimator_model(params):
+    model_params = {'init': 'do not know how to manage this piece of code...'}
+    if params.model == 'rnn':
+        if params.cell == 'lstm':
+            model_params['rnn'] = rnn_model.lstm_block
+        if params.cell == 'gru':
+            model_params['rnn'] == rnn_model.gru_block
+        return rnn_model.rnn_generator(
+                model_params['rnn'],
+                params.state_size, 
+                params.num_layers, 
+                params.out_width, 
+                params.data_format
+                )
+    
+    if params.model == 'cnn':
+        model_params = {'block': resnet_model.bottleneck_block, 'layers': [3, 4, 6, 3]}
+        return resnet_model.n4b_resnet_v2_generator(
+                model_params['block'], 
+                model_params['layers'], 
+                params.out_width, 
+                params.data_format
+                )
